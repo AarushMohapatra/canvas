1d9fa600280494c02bfc5257413a36c5f314ac3f
AarushMohapatra <103845656+AarushMohapatra@users.noreply.github.com>
2022-08-13 12:50:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*get the canvas element using its id and store it in a variable “canvas” so that we can 
 manipulate the canvas throughout the js code.*/
-var canvas = document.getElementById("myCanvas")
+/*var canvas = document.getElementById("myCanvas")
 
 //Get reference of canvas created.
 
@@ -27,7 +27,8 @@ ctx.stroke();
 color = "green";
 ctx.beginPath();
 ctx.arc(400, 250, 40, 0, 2 * Math.PI);
-ctx.stroke();
+ctx.stroke();*/
+canvas = document.getElementById("myCanvas"); ctx= canvas.getContext("2d"); ctx.beginPath(); ctx.strokeStyle = "grey"; ctx.lineWidth = 1; ctx.rect(150, 143, 430, 200); ctx.stroke(); ctx.beginPath(); ctx.strokeStyle = "blue"; ctx.lineWidth = 5; ctx.arc(250, 210, 40 ,0 , 2 * Math.PI); ctx.stroke(); ctx.beginPath(); ctx.strokeStyle = "black"; ctx.lineWidth = 5; ctx.arc(350, 210, 40 ,0 , 2 * Math.PI); ctx.stroke(); ctx.beginPath(); ctx.strokeStyle = "red"; ctx.lineWidth = 5; ctx.arc(450, 210, 40 ,0 , 2 * Math.PI); ctx.stroke(); ctx.beginPath(); ctx.strokeStyle = "orange"; ctx.lineWidth = 5; ctx.arc(300, 250, 40 ,0 , 2 * Math.PI); ctx.stroke(); ctx.beginPath(); ctx.strokeStyle = "green"; ctx.lineWidth = 5; ctx.arc(400, 250, 40 ,0 , 2 * Math.PI); ctx.stroke();
 // Attach canvas and addEventListener with 'mousedown' event.
 
 canvas.addEventListener("mousedown", my_mousedown);
